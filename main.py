81eb230fe097ff06c487c51bc90c8baacc9839d7
apqint <128084939+apqint@users.noreply.github.com>
2023-03-16 20:02:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys, os
+import asyncio
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import time
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("cal")
+        self.setGeometry(100, 100, 980, 720)
+        self.show()
+
+app = QApplication(sys.argv)
+
+if __name__ == "__main__":
+	window = MainWindow()
+	window.show()
+	app.exec()
+
