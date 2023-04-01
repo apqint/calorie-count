bebeb67b73055342e3ea95a0e8583126d0535a5c
apqint <128084939+apqint@users.noreply.github.com>
2023-04-01 23:55:02 +0300

    Update food.py

--- src/food.py
+++ src/food.py
@@ -8,12 +8,16 @@ def search(hits, queue, key) -> list:
     payload = []
     for i in range(hits):
         current = data['foods'][i]
-        calories = [item for item in current['foodNutrients'] if item['nutrientName']=='Energy'][0]['value']
+        try: calories = [item for item in current['foodNutrients'] if item['nutrientName']=='Energy' and item['unitName'].lower()=="kcal"][0]['value']
+        except: calories = 0
+        try: protein = [item for item in current['foodNutrients'] if item['nutrientName']=='Protein' and item['unitName'].lower()=="g"][0]['value']
+        except: protein = 0
         payload.append({
             "id": current['fdcId'],
             "description": current['description'].lower().title(),
             "category": current['foodCategory'],
             "calories": calories,
+            "protein": protein
             })
         if "brandName" in current or "brandOwner" in current:
             payload[i]["brand"]=current['brandName'] if "brandName" in current else current['brandOwner']
@@ -21,12 +25,13 @@ def search(hits, queue, key) -> list:
     return payload
 
 class Food:
-    def __init__(self, name, _id, amount, key) -> None:
+    def __init__(self, name, _id, amount, key, nutrition) -> None:
         self.name = name
         self._id = _id
         self.key = key
         self.amount = amount
-        self.createNutritionalData()
+        self.calories = nutrition[0] # PER 100g
+        self.protein = nutrition[1] # PER 100g, CALCULATIONS DONE LATER
 
     def createNutritionalData(self) -> None:
         req = requests.get(f"https://api.nal.usda.gov/fdc/v1/food/{self._id}?api_key={self.key}")
@@ -48,3 +53,24 @@ class Food:
                 pass
         payload['calories'] = [data['foodNutrients'][2]['amount'], 'kcal']
         self.payload = payload
+
+class Meal:
+    def __init__(self, name):
+        self.name = name
+        self.food_list = []
+
+    def append(self, thing):
+        self.food_list.append(thing)
+
+    def pop(self, thing):
+        self.food_list.pop(thing)
+
+    def setName(self, new_name):
+        self.name = new_name
+
+    def getFoods(self):
+        return self.food_list
+
+    def getName(self):
+        return self.name
+
